2780f1a79a2b6108fbe14517f4f15eaf4cdb61b2
Joao Victor Benedet Machado <joao.193922@alunosatc.edu.br>
2024-08-02 17:15:52 -0300

    novos bgls

--- sql_comandos/dml001.sql
+++ sql_comandos/dml001.sql
@@ -14,3 +14,9 @@ INSERT INTO curso(codigo, nome) values(1, 'informatica')
 
 INSERT INTO aluno(codigo, nome, endereco, nomepai, nomemae, cpf, codcurso, codcidade, datanasci, telefone, email)
 values(1, 'Joao Victor Benedet Machado', 'Rua Campo Alegre, Nss Sra da Salete', 'Rodrigo Machado', 'Pamela Felisberto' , '244.245.249-23', 1, 1, 1, '21/07/2007', '48999414409', 'jvb3187@gmail.com')
+
+INSERT INTO aluno(codigo, nome, endereco, nomepai, nomemae, cpf, codcurso, codcidade, datanasci, telefone, email)
+VALUES (2, 'Maria Clara Silva', 'Avenida das Palmeiras, 123', 'Carlos Silva', 'Ana Silva', '123.456.789-01', 1, 2, '2006-09-15', '11987654321', 'maria.silva@example.com');
+
+INSERT INTO aluno(codigo, nome, endereco, nomepai, nomemae, cpf, codcurso, codcidade, datanasci, telefone, email)
+VALUES (4, 'Ana Luiza Costa', 'Praça Central, 789', 'Ricardo Costa', 'Juliana Costa', '321.654.987-23', 1, 1, '2008-06-25', '48987765432', 'ana.costa@example.com');
